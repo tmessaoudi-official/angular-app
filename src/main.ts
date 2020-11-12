f2e659f779e0b15f25ff7e9c8e65954647f3ccb1
Takieddine Messaoudi <tmessaoudi@smart-team.tn>
2020-11-12 11:35:10 +0100

    register home locale (dummy)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { registerLocaleData } from '@angular/common';
+const localeDummy = require(`@angular/common/locales/${process.env.APP_I18N_LOCALE_DEFAULT}`).default;
+const localeDummyExtra = require(`@angular/common/locales/extra/${process.env.APP_I18N_LOCALE_DEFAULT}`).default;
+
+registerLocaleData(localeDummy, `${process.env.APP_I18N_LOCALE_DUMMY}`, localeDummyExtra);
+
 if (environment.production) {
   enableProdMode();
 }

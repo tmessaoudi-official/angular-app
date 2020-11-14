ece97966fa593fd4739218a8c579aa123ccd34c1
Takieddine Messaoudi <tmessaoudi@smart-team.tn>
2020-11-14 15:41:58 +0100

    exclude dotenv run from build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,6 @@ import { EnvironmentNaming } from './EnvironmentNaming';
 const environmentVariableHandler = new EnvironmentVariableHandler();
 const environmentNaming = new EnvironmentNaming();
 
-
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
